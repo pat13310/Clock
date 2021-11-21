183e5a7b231d2ee9ef1355cffd9d61e252c6939d
pat13310 <92207286+pat13310@users.noreply.github.com>
2021-11-21 22:17:36 +0100

    Add files via upload

---
+++ saint/saint.py
@@ -0,0 +1,140 @@
+angels1 = ["!Jour de l'an", "Basile", "*Geneviève", "*Odilon",
+           "Edouard", "*Mélaine", "Raymond", "Lucien",
+           "*Alix", "Guillaume", "*Pauline", "*Tatiana",
+           "*Yvette", "*Nina", "Rémi", "Marcel",
+           "*Roseline", "*Prisca", "Marius", "Sébastien",
+           "*Agnès", "Vincent", "Banard", "François de Sales",
+           "!Conversion de Paul", "*Paule", "*Angèle", "Thomas d'Aquin",
+           "Gildas", "*Martine", "*Marcelle"];
+
+angels2 = ["*Ella", "Présentation", "Blaise", "*Véronique",
+           "*Agathe", "*Gaston", "*Eugènie", "*Jacqueline",
+           "*Apolline", "Arnaud", "Notre Dame de Lourdes", "Félix",
+           "*Béatrice", "Valentin", "Claude", "*Julienne",
+           "Alexis", "*Bernadette", "Gabin", "*Aimée",
+           "Damien", "*Isabelle", "Lazare", "Modeste",
+           "Roméo", "Nestor", "*Honorine", "Romain"];
+
+angels3 = ["Albin", "Charles", "Gwenolé", "Roméo",
+           "Théophile", "*Agnès", "*Félicité", "Ryan",
+           "*Françoise", "Vivien", "*Rosine", "*Justine",
+           "*Léandre", "*Eve", "Louise", "*Bénédicte",
+           "Patrick", "Cyrille", "Joseph", "*Alexandra",
+           "Clémence", "*Léa", "*Rébecca", "*Catherine",
+           "Humbert", "*Larissa", "Albert", "Gontran",
+           "*Gladys", "Amédée", "*Acace"];
+
+angels4 = ["Hugues", "*Sandrine", "Richard", "Isidore",
+           "*Irène", "Marcellin", "Jean-Baptiste de la Salle", "*Julie",
+           "Gautier", "Fulbert", "Stanislas", "Jules",
+           "*Ida", "Maxime", "Paterne", "Benoit-Joseph",
+           "*Anicet", "Parfait", "*Emma", "*Odette",
+           "Anselme", "Alexandre", "Georges", "Fidèle",
+           "Marc", "*Alida", "*Zita", "*Valérie",
+           "Catherine de Sienne", "Robert"];
+
+angels5 = ["!Fête du travail", "Boris", "Philippe-Jacques", "Sylvain",
+           "*Judith", "*Prudence", "*Gisèle", "!Armistice 1945",
+           "!Pac*me", "*Solange", "*Estelle", "Achille",
+           "*Rolande", "Mathias", "*Denise", "Honoré",
+           "Pascal", "Eric", "Yves", "Bernardin",
+           "Constantin", "Emile", "Didier", "Donatien",
+           "*Sophie", "Bérenger", "Augustin", "Germain",
+           "Aymar", "Ferdinand", "!Visitation de la Ste Vierge"];
+
+angels6 = ["Justin", "Alcide",
+           "Kevin", "*Clothide", "Boniface", "Claude", "Gilbert", "Médard", "Ephrem",
+           "Landry", "Barnabé", "Guy", "Antoine", "*Elisée", "*Germaine", "Régis",
+           "Hervé", "*Léonce", "*Jude", "*Silvère", "Louis", "Alban", "*Audrey",
+           "Jean", "Prosper", "Anthelme", "Fernand", "*Irénée", "Pierre", "Martial"];
+
+angels7 = ["Thierry", "*Eugénie", "Thomas", "*Isabelle", "Antoine-Marie", "*Maria", "Roger",
+           "*Priscille", "*Irma", "Ulric", "Benoit", "Olivier", "Henri", "Camille",
+           "boneaventure", "*Carmen", "*Charlotte", "Frédéric", "Arsène", "Aurèle", "Victor",
+           "*Madeleine", "*Brigitte", "*Christine", "Christophe", "*Anne", "Félix", "Samson",
+           "*Marthe", "*Juliette", "Ignace"];
+
+angels8 = ["Alphonse", "Julien", "*Lydie", "*Jean-Marie",
+           "Abel", "Sauveur", "Ga&tan", "Dominique", "Amour", "Laurent", "*Claire",
+           "*Chantal", "Hyppolyte", "Evrard", "Assomption", "*Armel", "*Hyacinthe",
+           "*Hélène", "Jean Eudes", "Bernard", "Christophe", "Fabrice", "*Rose de Lima", "Barthélémy", "Louis",
+           "Louis", "*Natacha", "*Monique", "Augustin", "*Sabine", "Fiacre", "Aristide"];
+
+angels9 = ["Gilles", "*Ingrid", "Grégoire", "*Rosalie",
+           "*Raïssa", "Bertrand", "*Reine", "!Nativité",
+           "Alain", "*Inès", "Adelphe", "Apollinaire",
+           "Aimé", "!Croix Glorieuse", "Roland", "*Edith",
+           "Renaud", "*Nadège", "*Emilie", "Davy",
+           "Matthieu", "Maurice", "!Automne", "Thècle",
+           "Hermann", "Côme et Damien", "Vincent de Paul", "Venceslas",
+           "Michel", "Jérôme"];
+
+angels10 = ["Thérèse de l'Enfant Jésus", "Léger", "Gérard", "François d'Assise",
+            "*Fleur", "Bruno", "Serge", "*Pélagie",
+            "Denis", "*Ghislaine", "Firmin", "Wilfried",
+            "Géraud", "Juste", "*Thérèse d'Avila", "*Edwige",
+            "Baudoin", "Luc", "René", "*Adeline",
+            "*Céline", "*Elodie", "Jean de Capistran", "Florentin",
+            "Crépin", "Dimitri", "*Emeline", "Jude",
+            "Narcisse", "Bienvenue", "Quentin"];
+
+angels11 = ["!Toussaint", "!Défunts", "Hubert", "Charles",
+            "*Sylvie", "*Bertille", "*Carine", "Geoffroy",
+            "Théodore", "Léon", "!Armistice 1918", "Christian",
+            "Brice", "Sidoine", "Albert", "*Marguerite",
+            "*Elisabeth", "Aude", "Tanguy", "Edmond",
+            "!Présence de Marie", "*Cécile", "Clément", "*Flora",
+            "*Catherine", "*Delphine", "Sévrin", "Jacques de la Marche",
+            "Saturnin", "André"];
+
+angels12 = ["*Florence", "*Viviane", "François Xavier", "*Barbara",
+            "Gérald", "Nicolas", "Ambroise", "!Immaculée Conception",
+            "Pierre Fourier", "Romaric", "Daniel", "!Jeanne-Françoise de Chan",
+            "*Lucie", "Gatien", "*Ninon", "*Alice",
+            "Ga&l", "*Aude", "Armand", "*Adèle",
+            "!Hiver", "*Françoise Xavière", "Clément", "*Flora",
+            "!No&l", "Etienne", "Jean", "Innocents",
+            "David", "Roger", "Sylvestre"];
+
+
+def get_Saint(month, day):
+    saint = "St Glinglin"
+    tab_saint = None
+    if month == 1:
+        tab_saint = angels1
+    if month == 2:
+        tab_saint = angels2
+    if month == 3:
+        tab_saint = angels3
+    if month == 4:
+        tab_saint = angels4
+    if month == 5:
+        tab_saint = angels5
+    if month == 6:
+        tab_saint = angels6
+    if month == 7:
+        tab_saint = angels7
+    if month == 8:
+        tab_saint = angels8
+    if month == 9:
+        tab_saint = angels9
+    if month == 10:
+        tab_saint = angels10
+    if month == 11:
+        tab_saint = angels11
+    if month == 12:
+        tab_saint = angels12
+
+    if tab_saint:
+        saint = tab_saint[int(day) - 1]
+
+        if not "!" in saint and not "*" in saint:
+            saint = "St " + saint
+
+        saint=saint.replace("!", "").replace("*", "Ste ")
+
+    return saint
+
+
+if __name__ == "__main__":
+    print(get_Saint(11, 6))
---
+++ start_clock.py
@@ -0,0 +1,103 @@
+from PyQt5 import QtCore, QtWidgets
+from datetime import datetime
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QGraphicsDropShadowEffect, QMainWindow
+from clock.Clock import Ui_Clock
+
+import sys
+from clock.saint.saint import get_Saint
+
+
+class ClockScreen(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_Clock()
+        self.ui.setupUi(self)
+        # suppression de la barre des titres
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        # effet ombrage
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(10)
+        self.shadow.setXOffset(2)
+        self.shadow.setYOffset(2)
+        self.shadow.setColor(QColor(0, 0, 0, 220))
+        self.ui.frame.setGraphicsEffect(self.shadow)
+
+        # QTIMER => START
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update_clock)
+        self.timer.start(1000)
+        self.old_day = -1
+
+        self.ui.pushButton_close.clicked.connect(self.close_window)
+
+
+    def close_window(self):
+        self.close()
+
+    def update_clock(self):
+        n = datetime.now()
+        hour = n.strftime("%H")
+        minute = n.strftime("%M")
+        second = n.strftime("%S")
+
+        style = "<span style=\" font-size:72pt;\">{hour}:{minute}:</span><span style=\" font-size:48pt;  color:#5d5d5d;\">{second}</span>"
+        strTime = style.replace('{hour}', hour).replace('{minute}', minute).replace('{second}', second)
+        self.ui.label_clock.setText(strTime)
+        style = "<span style=\" font-size:72pt;color:#d9d9d9\">{hour}:{minute}:</span><span style=\" font-size:48pt;  color:#d9d9d9;\">{second}</span>"
+        strTime = style.replace('{hour}', hour).replace('{minute}', minute).replace('{second}', second)
+        self.ui.label_shadow.setText(strTime)
+
+        jour_semaine = self.jour_semaine()
+        jour = n.strftime("%d")
+        month = n.strftime("%m")
+        year = n.strftime("%Y")
+        if jour != self.old_day:
+            m = self.get_month_name(month)
+            self.ui.label_date.setText(f"{jour_semaine} {jour} {m} {year}")
+            self.old_day = jour
+
+            style="<span style = \"font-style:italic; color:#5a5a5a;\" >{saint}< / span >"
+            saint= get_Saint(int(month),jour)
+            style=style.replace("{saint}",saint)
+            self.ui.label_saint.setText(style)
+
+    def jour_semaine(self):
+        day = {0: "Lundi",
+               1: "Mardi",
+               2: "Mercredi",
+               3: "Jeudi",
+               4: "Vendredi",
+               5: "Samedi",
+               6: "Dimanche"
+               }
+        n = datetime.now().weekday()
+        return day.get(n)
+
+    def get_month_name(self, month):
+        tab_month = {1: "Janvier",
+                     2: "Février",
+                     3: "Mars",
+                     4: "Avril",
+                     5: "Mai",
+                     6: "Juin",
+                     7: "Juillet",
+                     8: "Août",
+                     9: "Septembre",
+                     10: "Octobre",
+                     11: "Novembre",
+                     12: "Décembre"
+                     }
+        return tab_month.get(int(month))
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    application = ClockScreen()
+    application.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
